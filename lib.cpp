650a0c1718cc8e9f7c60afa9a1975af999881cb6
alessiocarrea <118049896+alessiocarrea@users.noreply.github.com>
2022-12-13 11:13:19 +0100

    Update lib.cpp

--- lib.cpp
+++ lib.cpp
@@ -1,1 +1,13 @@
-
+#include "lib.h"
+float massimo(float a[],int dim) {
+    float max;
+    if (dim>0) {
+      max = a[0];
+      for (int i=0; i < dim; i++) {
+          if (a[i] > max) {
+              max = a[i];
+          }
+      }
+    }
+    return max;
+}
